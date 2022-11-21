86c84616791fbd12f7469edf38a967fb06379408
jeffreyaven <javen@avensolutions.com>
2022-11-21 12:39:04 +1100

    added variable expansion/substitution

--- extensions/stackql.py
+++ extensions/stackql.py
@@ -1,25 +1,41 @@
+from __future__ import print_function
 import pandas as pd
-from io import StringIO
 import psycopg2, json
 from psycopg2.extras import RealDictCursor
+from IPython.core.magic import (Magics, magics_class, line_cell_magic)
+from io import StringIO
+from string import Template
 
 conn = psycopg2.connect("dbname=stackql user=stackql host=localhost port=5444")
 
-def run_query(query):
-    cur = conn.cursor(cursor_factory=RealDictCursor)
-    cur.execute(query)
-    rows = cur.fetchall()
-    cur.close()
-    return json.dumps(rows)
+@magics_class
+class StackqlMagic(Magics):
 
-def stackql(line, cell):
-    query = StringIO(cell)
-    return pd.read_json(run_query(query.read()))
+    def get_rendered_query(self, data):
+        t = Template(StringIO(data).read())
+        rendered = t.substitute(self.shell.user_ns)
+        return rendered
+
+    def run_query(self, query):
+        cur = conn.cursor(cursor_factory=RealDictCursor)
+        cur.execute(query)
+        rows = cur.fetchall()
+        cur.close()
+        return pd.read_json(json.dumps(rows))
+
+    @line_cell_magic
+    def stackql(self, line, cell=None):
+        if cell is None:
+            results = self.run_query(self.get_rendered_query(line))
+        else:
+            results = self.run_query(self.get_rendered_query(cell))
+        return results            
 
 def load_ipython_extension(ipython):
-    """This function is called when the extension is
-    loaded. It accepts an IPython InteractiveShell
-    instance. We can register the magic with the
-    `register_magic_function` method of the shell
-    instance."""
-    ipython.register_magic_function(stackql, 'cell')
+    """
+    Any module file that define a function named `load_ipython_extension`
+    can be loaded via `%load_ext module.path` or be configured to be
+    autoloaded by IPython at startup time.
+    """
+    ipython.register_magics(StackqlMagic)
+
