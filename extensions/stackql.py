a2b0121206c16098044aae311bca4758f90d2128
Jeffrey Aven <javen@avensolutions.com>
2023-08-22 17:07:05 +1000

    updates

--- extensions/stackql.py
+++ extensions/stackql.py
@@ -1,53 +1,97 @@
-from __future__ import print_function
+"""
+This module provides IPython magic for querying a StackQL database.
+
+The primary functionality is exposed through the `stackql` cell magic
+which allows querying the database and returning the results as a Pandas DataFrame.
+
+Attributes:
+    conn (psycopg2.extensions.connection): Connection to the StackQL database.
+
+Classes:
+    StackqlMagic: Defines the `stackql` cell magic.
+
+Functions:
+    load_ipython_extension(ipython): Register the magic with IPython.
+"""
+
 import pandas as pd
-import psycopg2, json, nbformat
+import psycopg2
+import json
 from psycopg2.extras import RealDictCursor
-from IPython.core.magic import (Magics, magics_class, line_cell_magic)
-from io import StringIO
+from IPython.core.magic import (Magics, magics_class, cell_magic)
 from string import Template
-from IPython.core.interactiveshell import InteractiveShell
+import argparse
 
 conn = psycopg2.connect("dbname=stackql user=stackql host=localhost port=5444")
 
 @magics_class
 class StackqlMagic(Magics):
-
-    def get_rendered_query(self, data):
-        t = Template(StringIO(data).read())
-        rendered = t.substitute(self.shell.user_ns)
-        return rendered
-
-    def run_query(self, query):
-        cur = conn.cursor(cursor_factory=RealDictCursor)
-        cur.execute(query)
-        rows = cur.fetchall()
-        cur.close()
-        return pd.read_json(json.dumps(rows))
-
-    @line_cell_magic
+    """
+    Defines the `stackql` cell magic.
+    
+    This class provides an IPython cell magic, `stackql`, which allows querying
+    a StackQL database and returning the results as a Pandas DataFrame.
+    
+    The magic also supports argument parsing to control its behavior:
+    - `--no-display`: If specified, the results won't be displayed.
+    """
+    
+    @cell_magic
     def stackql(self, line, cell=None):
-        if cell is None:
-            results = self.run_query(self.get_rendered_query(line))
-        else:
-            results = self.run_query(self.get_rendered_query(cell))
-        return results
+        """
+        The `stackql` cell magic queries the StackQL database using the provided SQL
+        in the cell. The results are returned as a Pandas DataFrame and 
+        optionally displayed based on the arguments.
+        
+        Args:
+            line (str): The arguments for the magic.
+            cell (str, optional): The SQL query to run.
+        
+        Returns:
+            pd.DataFrame: The results of the query, if `--no-display` is not specified.
+        """
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--no-display", action="store_true", help="Do not display the result.")
+        args = parser.parse_args(line.split())
+        
+        # Fetch args_json from the environment
+        arguments = json.loads(self.shell.user_ns.get("args_json", "{}"))
+        
+        # Substitute the arguments into the query
+        rendered_query = Template(cell).substitute(arguments)
 
-    @line_cell_magic
-    def run(self, line, cell=None):
-        """Run a notebook file within the current IPython shell."""
-        path = line.strip()
-        with open(path) as f:
-            notebook = nbformat.read(f, as_version=4)
-            for cell in notebook.cells:
-                if cell.cell_type == 'code':
-                    code = cell.source
-                    # execute the code in the main IPython shell
-                    self.shell.ex(code)
+        results = self.run_query(rendered_query)
+        
+        # Assigning results to a variable in the global namespace
+        self.shell.user_ns['stackql_df'] = results
+        
+        # Conditionally display results
+        if not args.no_display:
+            return results
+    
+    def run_query(self, query):
+        """
+        Executes the given SQL query against the StackQL database.
+        
+        Args:
+            query (str): The SQL query to execute.
+        
+        Returns:
+            pd.DataFrame: The results of the query as a DataFrame.
+        """
+        with conn.cursor(cursor_factory=RealDictCursor) as cur:
+            cur.execute(query)
+            rows = cur.fetchall()
+            return pd.read_json(json.dumps(rows))
 
 def load_ipython_extension(ipython):
     """
-    Any module file that defines a function named `load_ipython_extension`
-    can be loaded via `%load_ext module.path` or be configured to be
-    autoloaded by IPython at startup time.
+    Registers the `StackqlMagic` class with IPython.
+    
+    This function is automatically called by IPython when using 
+    `%load_ext ext.stackql`.
+    
+    Args:
+        ipython (InteractiveShell): The IPython shell instance.
     """
     ipython.register_magics(StackqlMagic)
