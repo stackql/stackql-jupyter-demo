579f58702a0948c15ab2ac81576d47ddcf4989b9
Jeffrey Aven <javen@avensolutions.com>
2023-08-28 19:54:41 +1000

    google network audit query updates

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 /srv/stackql/stackql --version
 echo "starting stackql server..."
-# nohup /srv/stackql/stackql --auth="${STACKQL_PROVIDER_AUTH}" --pgsrv.port=5444 srv &
-nohup /srv/stackql/stackql --pgsrv.port=5444 srv &
+nohup /srv/stackql/stackql --http.response.pageLimit=-1 --pgsrv.port=5444 srv &
 echo "stackql server started"
 start-notebook.sh --NotebookApp.token=''
