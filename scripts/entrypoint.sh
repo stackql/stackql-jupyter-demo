342fdbe381b7c9c9832292eab8c791ce74b69933
Jeffrey Aven <javen@avensolutions.com>
2023-12-27 16:04:51 +1100

    added benchmark

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 /srv/stackql/stackql --version
 echo "starting stackql server..."
-nohup /srv/stackql/stackql --http.response.pageLimit=-1 --pgsrv.port=5466 srv &
+nohup /srv/stackql/stackql --execution.concurrency.limit=-1 --http.response.pageLimit=-1 --pgsrv.port=5466 srv &
 echo "stackql server started"
 start-notebook.sh --NotebookApp.token=''
